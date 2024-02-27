d1c886a778f0ef5ac39e83a5dee166c53ca91f37
Vasandr15 <vasandrey007@gmail.com>
2024-02-27 11:29:47 +0700

    phone number added

--- roomskeyFrontend/src/pages/RegistrationPage/RegisterPage.jsx
+++ roomskeyFrontend/src/pages/RegistrationPage/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import {Form, Input, Button, Card, Flex, Typography} from 'antd';
+import InputMask from 'react-input-mask';
 import styles from './register.module.css'
 import {Validations} from "../../consts/validations.js";
 
@@ -16,6 +17,8 @@ const RegistrationForm = () => {
             document.body.classList.remove(styles.backgroundImage);
         }
     }, []);
+
+
     const onFinish = (values) => {
         setLoading(true);
         console.log('Received values of form:', values);
@@ -45,6 +48,12 @@ const RegistrationForm = () => {
                     </Flex>
                     <Form.Item name="email" label="Email" rules={Validations.emailValidation()}>
                         <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        name="phone" label="Номер телефона" rules={Validations.phoneValidation()}>
+                        <InputMask mask="+7 (999) 999-99-99" maskChar=" " alwaysShowMask={true} onChange={e => console.log(e.target.value)} >
+                            {(inputProps) => <Input {...inputProps} />}
+                        </InputMask>
                     </Form.Item>
                     <Form.Item name="password" label="Пароль" hasFeedback rules={Validations.passwordValidation()}>
                         <Input.Password/>
