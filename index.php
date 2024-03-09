9bbd97097b8254216a324be5a9cc4b83d7b4ffac
Archi <129053616+OG-UZOK@users.noreply.github.com>
2024-03-09 17:09:49 +0700

    almost ALL
    Осталось дождаться проверки от Димы

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+    // include_once 'helpers/validation.php';
+    include_once 'roomskeyBackend/helperFunctions/headers.php';
+    // include_once 'helpers/searchAdres.php';
+
+    date_default_timezone_set('Asia/Tomsk');
+    
+    global $Link;
+
+    function getData($method)
+    {
+        $data = new stdClass();
+        if ($method != "GET")
+        {
+            $data -> body = json_decode(file_get_contents('php://input'));
+        }
+        $data->paramaters = [];
+            $dataGet = $_GET;
+            foreach ($dataGet as $key => $value)
+            {
+                if ($key != "q")
+                {
+                    $data->paramatersq[$key] = $value;
+                }
+            }
+            return $data;
+    }
+
+    function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    header('Content-type: application/json');
+
+    $host = '79.133.183.21';
+    $dbname = 'keysHandler';
+    $username = 'postgres';
+    $password = 'bezdar123';
+
+    // $host = 'localhost';
+    // $dbname = 'db';
+    // $username = 'postgres';
+    // $password = 'postgres';
+
+    $conn_string = "host=$host dbname=$dbname user=$username password=$password";
+
+    $Link = pg_connect($conn_string);
+
+    if (!$Link) {
+        setHTTPStatus("500", "Ошибка подключения к базе данных.\n");
+        exit;
+    }
+
+    $url = isset($_GET['q']) ? $_GET['q'] : '';
+    
+    $url = rtrim($url, '/');
+    $urlList = explode('/', $url);
+
+    $object = $urlList[1];
+    $router = $urlList[2];
+    $requestData = getData(getMethod());
+    $method = getMethod();
+
+
+    if (file_exists(realpath(dirname(__FILE__)).'/'.$url.'.php'))
+    {       
+        include_once $url.'.php';
+        route($method,$urlList,$requestData);
+    }
+    else if (file_exists(realpath(dirname(__FILE__)).'/API/'.$object.'.php'))
+    {
+        include_once 'API/'.$object.'.php';
+        route($method,$urlList,$requestData);
+    }
+    else
+    {
+        setHTTPStatus("404", "Not Found");
+    }
+
+    pg_close($Link);
+    return;
---
+++ roomskeyBackend/getMyKeys.php
@@ -0,0 +1,119 @@
+<?php
+
+include_once 'helperFunctions/headers.php';
+    
+function validatePage($page, $count)
+{
+    if ($page <= $count || ($page == 1 && $count == 0))
+    {
+        return true;
+    }
+    return false;
+}
+
+
+function route($method, $urlList, $requestData)
+{
+    global $Link;
+    switch($method)
+    {
+        case 'GET':
+
+            $page = $_GET["page"] ?? 1;
+            $size = $_GET["size"] ?? 10;
+
+            if (!is_numeric($page) or $page < 1)
+            {
+                setHTTPStatus('400', 'Invalid value for attribute page');
+                return;
+            }
+    
+            if (!is_numeric($size) || $size < 1)
+            {
+                setHTTPStatus('400', 'Invalid value for attribute size');
+                return;
+            }
+
+            $headers = getallheaders();
+            $token = substr($headers['Authorization'], 7);
+            if (!$token){
+                setHTTPStatus("401", "Unauthorized");
+                break;
+            }
+            
+            // Подключение к базе данных
+            
+            $userIDQuery = "SELECT userid FROM tokens WHERE token = '$token'";
+            $userIDResult = pg_query($Link, $userIDQuery);
+
+                    
+            // Получение ID пользователя из результата запроса
+            $userFromToken = pg_fetch_assoc($userIDResult);
+            if (!is_null($userFromToken)) {
+                $userID = $userFromToken['userid'];
+            
+                $keysQuery = "SELECT id,room,building FROM keys WHERE user_id = '$userID'";
+                $keysResult = pg_query($Link, $keysQuery);
+                
+
+                $keys = array();
+                if ($keysResult) {
+       
+                    while ($keyRow = pg_fetch_assoc($keysResult)) {
+                        $keyId = trim($keyRow['id']);
+                    
+                        // Формирование ключа, если его еще нет в массиве
+                        if (!array_key_exists($keyId, $keys)) {
+                            $keys[$keyId] = array(
+                                'id' => $keyId,
+                                'room' => $keyRow['room'],
+                                'building' => $keyRow['building']
+                            );
+                        }
+                    }
+
+                    $keys = array_values($keys);
+
+                    $paginatedKeys = array_chunk($keys, $size, true);
+
+                    // Получаем данные только для текущей страницы
+                    $currentKeys = $paginatedKeys[$page - 1];
+
+                    $count = count($keys);
+                    $pageCount = ceil($count / $size);
+
+                    if (!validatePage($page, $pageCount))
+                    {
+                        setHTTPStatus('400', 'Invalid value for attribute page');
+                        return;
+                    }
+
+                    $output = array(
+                        'keys' => $currentKeys,
+                        'pagination' => array(
+                            'size' => $size,
+                            'count' => $pageCount,
+                            'current' => $page
+                        )
+                    );
+                
+                    echo json_encode($output);
+                } else {
+                echo "Ошибка при выполнении запроса: " . pg_last_error($Link);
+                }
+                
+            }
+            else{
+                setHTTPStatus("401", "Unauthorized");
+            }
+
+            break;
+
+        break;
+    default:
+        setHTTPStatus("400", "You can only use GET");
+        break;
+    }
+
+}
+?>
